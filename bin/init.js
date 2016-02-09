ab2f7c1e9955693af4de7368fe419509f809ebf5
Mayank Chandola <imayankchd@gmail.com>
2016-02-09 13:09:23 +0530

    Add tests and ci

--- bin/init.js
+++ bin/init.js
@@ -9,7 +9,7 @@ require('shelljs/global');
 
 program
 .description('Create a MERN app in current directory!')
-.option('-i, --init [type]', 'Init a new mern boilerplate')
+.option('-i, --init [name]', 'Init a new mern boilerplate')
 .parse(process.argv);
 
 
@@ -17,6 +17,12 @@ if (!which('git')) {
   console.log(chalk.red('Sorry, this script requires git'));
   exit(1);
 }
+
+if(! program.init) {
+  console.log(chalk.cyan('Please pass correct options or type --help or -h for help!'));
+  exit(1);
+}
+
 
 if(program.init && program.init !== true) {
  if(test('-d', program.init)) {
@@ -30,7 +36,7 @@ if(program.init && program.init !== true) {
  exec('git init');
  
  var interval = setInterval(function() {
-   logUpdate("Fetching the boilerplate..." + chalk.cyan.bold.dim(frame()));
+   logUpdate("Fetching the boilerplate..." + chalk.cyan.bold(frame()));
  }, 50)
  
  var e = exec('git pull https://github.com/Hashnode/mern-starter.git', function(code, stdout, stderr) {
---
+++ tests/test.js
@@ -0,0 +1,20 @@
+import test from 'ava';
+import path from 'path';
+import childProcess from 'child_process';
+import pify from 'pify';
+
+const exec = pify(childProcess.exec);
+
+
+test('shows help on --help', async t => {
+	const stdout = await exec('../bin/init.js --help');
+	t.is(stdout.trim(), 'Usage: init [options]\n\n  Create a MERN app in current directory!\n\n  Options:\n\n    -h, --help         output usage information\n    -i, --init [type]  Init a new mern boilerplate');
+});
+
+
+test('shows help on --h', async t => {
+	const stdout = await exec('../bin/init.js --help');
+	t.is(stdout.trim(), 'Usage: init [options]\n\n  Create a MERN app in current directory!\n\n  Options:\n\n    -h, --help         output usage information\n    -i, --init [type]  Init a new mern boilerplate');
+});
+
+
