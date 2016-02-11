e103e3d361b2e1b2587f9792064eab968864e857
Mayank Chandola <imayankchd@gmail.com>
2016-02-11 10:27:49 +0530

    fix tests

--- tests/test.js
+++ tests/test.js
@@ -8,13 +8,13 @@ const exec = pify(childProcess.exec);
 
 test('shows help on --help', async t => {
 	const stdout = await exec('../bin/init.js --help');
-	t.is(stdout.trim(), 'Usage: init [options]\n\n  Create a MERN app in current directory!\n\n  Options:\n\n    -h, --help         output usage information\n    -i, --init [type]  Init a new mern boilerplate');
+	t.is(stdout.trim(), 'Usage: init [options]\n\n  Create a MERN app in current directory!\n\n  Options:\n\n    -h, --help  output usage information');
 });
 
 
 test('shows help on --h', async t => {
 	const stdout = await exec('../bin/init.js --help');
-	t.is(stdout.trim(), 'Usage: init [options]\n\n  Create a MERN app in current directory!\n\n  Options:\n\n    -h, --help         output usage information\n    -i, --init [type]  Init a new mern boilerplate');
+	t.is(stdout.trim(), 'Usage: init [options]\n\n  Create a MERN app in current directory!\n\n  Options:\n\n    -h, --help  output usage information');
 });
 
 
