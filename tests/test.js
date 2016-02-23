d4a1c210374cfff587953c45bf429df51d15cf11
stephan281094 <stephan281094@gmail.com>
2016-02-23 18:35:50 +0100

    Fix code styling and improve console messages

--- tests/test.js
+++ tests/test.js
@@ -5,16 +5,12 @@ import pify from 'pify';
 
 const exec = pify(childProcess.exec);
 
-
 test('shows help on --help', async t => {
 	const stdout = await exec('../bin/init.js --help');
-	t.is(stdout.trim(), 'Usage: init [options]\n\n  Create a MERN app in current directory!\n\n  Options:\n\n    -h, --help  output usage information');
+	t.is(stdout.trim(), 'Usage: init [options]\n\n  Create a MERN app in the current directory.\n\n  Options:\n\n    -h, --help  output usage information');
 });
-
 
 test('shows help on --h', async t => {
 	const stdout = await exec('../bin/init.js --help');
-	t.is(stdout.trim(), 'Usage: init [options]\n\n  Create a MERN app in current directory!\n\n  Options:\n\n    -h, --help  output usage information');
+	t.is(stdout.trim(), 'Usage: init [options]\n\n  Create a MERN app in the current directory.\n\n  Options:\n\n    -h, --help  output usage information');
 });
-
-
