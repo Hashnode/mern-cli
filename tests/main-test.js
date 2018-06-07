2919c5f949d05ebabfc25ddf49250b006db48252
Manny Henri <manny@mannydesigns.com>
2018-06-07 10:13:25 -0400

    updated tests for travis

--- tests/main-test.js
+++ tests/main-test.js
@@ -6,9 +6,16 @@ const exec = pify(childProcess.exec);
 
 
 test('shows help on --help', async (t) => {
-    const stdout = await exec('../bin/mern.js --help');
+    const stdout = await exec('./bin/mern.js --help');
     t.is(stdout.trim(), `Usage: mern [options] [command]
 
+  Initialize a MERN powered project
+
+  Options:
+
+    -V, --version  output the version number
+    -v, --version  check version
+    -h, --help     output usage information
 
   Commands:
 
@@ -16,36 +23,5 @@ test('shows help on --help', async (t) => {
     list           List MERN variants
     search [term]  Search for MERN variant
     info [term]    View details of a MERN variant
-    help [cmd]     display help for [cmd]
-
-  Initialize a MERN powered project
-
-  Options:
-
-    -h, --help     output usage information
-    -V, --version  output the version number
-    -v, --version  check version`);
-});
-
-
-test('shows help on --h', async (t) => {
-    const stdout = await exec('../bin/mern.js --help');
-    t.is(stdout.trim(), `Usage: mern [options] [command]
-
-
-  Commands:
-
-    init [name]    Initialize a MERN project.
-    list           List MERN variants
-    search [term]  Search for MERN variant
-    info [term]    View details of a MERN variant
-    help [cmd]     display help for [cmd]
-
-  Initialize a MERN powered project
-
-  Options:
-
-    -h, --help     output usage information
-    -V, --version  output the version number
-    -v, --version  check version`);
+    help [cmd]     display help for [cmd]`);
 });
